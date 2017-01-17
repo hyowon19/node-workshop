58c813d67443a8b3db88647ccf54e6855435e824
Victor Wong <vj1901@hotmail.com>
2017-01-17 19:46:25 +0000

    cleaned code

--- number-guessing-game.js
+++ number-guessing-game.js
@@ -21,11 +21,12 @@ var startGame = function() {
         game.count++;
         game.state = true;
         if(game.state){
-             startGame()
+             startGame();
         }
-    } else {
+    }
+    else {
         var actualNumber = Number(result['Guess a number between 1 and 100!']);
-        console.log("actual: ",actualNumber," random: ", guessThisNumber)
+        console.log("actual: ",actualNumber," random: ", guessThisNumber);
         if (actualNumber === guessThisNumber) {
             game.count++;
             console.log("Congratulations, you have guessed the correct number!");
@@ -39,30 +40,30 @@ var startGame = function() {
             game.count++;
             game.state = true;
             if(game.state){
-                startGame()
+                startGame();
             }
         } else if (actualNumber < 1 || actualNumber > 100) {
             console.log("You guessed a number that was not between 1 to 100.  Please try again.");
             game.count++;
             game.state = true;
             if(game.state){
-                startGame()
+                startGame();
             }
         } else if (actualNumber > guessThisNumber) {
             console.log("You guessed too high.  Try guessing lower.");
             game.count++;
             game.state = true;
             if(game.state){
-                startGame()
+                startGame();
             }
         } else if (actualNumber < guessThisNumber) {
             console.log("You guessed too low.  Try guessing higher.");
             game.count++;
             game.state = true;
             if(game.state){
-                startGame()
+                startGame();
             }
-        }}
+        }}  
     })
 }
 
